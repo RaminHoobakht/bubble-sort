641691e57e558f2c109a19cf927ee8ace78ff8a8
hinata <hinatachikao1971@gmail.com>
2025-12-28 10:21:31 +0330

    very simple bubble sorting code using loop and for-loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    println!("\n");
+
+    let mut number_list = vec![101, 220, 301, 14, 50, 16, 72, 89, 19];
+    let len = number_list.len() - 1;
+
+    'outer: loop {
+        let mut swapped = false;
+
+        for index in 0..len {
+            if number_list[index] < number_list[index + 1] {
+                number_list.swap(index, index + 1);
+                swapped = true;
+            }
+        }
+
+        if !swapped {
+            break 'outer;
+        }
+    }
+
+    for item in number_list {
+        println!(" -> {}", item);
+    }
+
+    println!("\n The End ...\n");
 }
